bc51608af7daec921e47316304b47afa031e8e93
Ramani Nikunj <nikunjramani7624@gmail.com>
2020-08-23 13:41:33 +0530

    Initial commit

---
+++ app/src/main/java/com/hissab/Login/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hissab.Login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hissab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
